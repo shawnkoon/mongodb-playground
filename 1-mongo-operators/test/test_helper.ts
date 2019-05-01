6eca8080c6c6ecd51896b417000fd820cb9fd249
Shawn Choi <shawn.choi@us.imshealth.com>
2019-04-30 23:14:44 -0700

    Update repo structure && add lesson 1 topic

---
+++ 1-mongo-operators/test/test_helper.ts
@@ -0,0 +1,20 @@
+// Lib
+import mongoose from 'mongoose';
+
+before(async () => {
+  await mongoose.connect('mongodb://localhost:27017/users_test', {
+    useNewUrlParser: true
+  });
+
+  await mongoose.connection
+    .once('open', () => {})
+    .on('error', error => console.warn('Warning', error));
+});
+
+beforeEach(async () => {
+  try {
+    await mongoose.connection.collections['users'].drop();
+  } catch (ex) {
+    console.error('Error!', ex);
+  }
+});
